aa276afe7b88f28f23334967c75116704cb2a554
root-parth <69101209+root-parth@users.noreply.github.com>
2023-12-12 11:39:37 +0530

    Update ThreatHuntingScriptv1.1.ps1
    Added Logout functionality.

--- ThreatHuntingScriptv1.1.ps1
+++ ThreatHuntingScriptv1.1.ps1
@@ -64,3 +64,14 @@ $foundIOCs
 
 Write-Host -ForegroundColor Green "`nTotal of '$($notfoundIOCs.Count)' were NOT found in the log analytics workspace '$worksacenme'.`n"
 }
+
+$choice = Read-Host "Do you want to logout? (Y/N)"
+# Check the user's choice
+if ($choice -eq "Y" -or $choice -eq "y") {
+    # Execute the logout command
+    Disconnect-AzAccount
+    Write-Host "Logged out."
+} else {
+    # Print a message in red text
+    Write-Host -ForegroundColor Red "Please do not forget to logout once the threat-hunting is complete."
+}
